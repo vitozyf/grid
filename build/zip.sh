60fd8f3aa852b22a64bf963efdfa95289d5b57c3
vito <vitozhangyf@outlook.com>
2019-11-18 21:03:32 +0800

    zip

--- build/zip.sh
+++ build/zip.sh
@@ -5,7 +5,9 @@ cp -r dist/fonts/ dist/assets/vue/Grid/fonts
 
 cp -r dist/grid.css dist/grid.umd.min.js dist/grid.umd.min.js.map  dist/assets/vue/Grid
 
+cp -r dist/fonts/ /Users/zhangyongfeng/Code/ZnlBomAISNS/src/lib/Grid/fonts
 cp -r dist/grid.css dist/grid.umd.min.js dist/grid.umd.min.js.map  /Users/zhangyongfeng/Code/ZnlBomAISNS/src/lib/Grid
+
 
 cd ./dist/
 
